1d7fa6ccf486231ce96f9620515a5638d3a4c0a4
Tharungr2002 <tharungr2002@gmail.com>
2025-06-30 00:17:25 +0530

    Analytical services

---
+++ analyticalserive/src/main/java/com/example/analyticalserive/AnalyticalseriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.analyticalserive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticalseriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticalseriveApplication.class, args);
+	}
+
+}
---
+++ analyticalserive/src/test/java/com/example/analyticalserive/AnalyticalseriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.analyticalserive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticalseriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
