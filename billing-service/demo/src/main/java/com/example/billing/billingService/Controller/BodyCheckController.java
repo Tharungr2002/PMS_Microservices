6479b82f9310aa1a970561ac1178a7438be2fc89
Tharun gr <tharungr2002@gmail.com>
2025-10-11 14:23:06 +0530

    Apntm billing integration

--- billing-service/demo/src/main/java/com/example/billing/billingService/Controller/BodyCheckController.java
+++ billing-service/demo/src/main/java/com/example/billing/billingService/Controller/BodyCheckController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("admin/bodycheck")
+@RequestMapping("admin/inventory/bodycheck")
 public class BodyCheckController {
 
     @Autowired
--- billing-service/demo/src/main/java/com/example/billing/billingService/Controller/MedicineController.java
+++ billing-service/demo/src/main/java/com/example/billing/billingService/Controller/MedicineController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("admin/medicine")
+@RequestMapping("admin/inventory/medicine")
 public class MedicineController {
 
     @Autowired
